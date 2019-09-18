3316141dbc77adccdc36479fc7e76c767f814641
Victoria Sauder <vsauder@grace6.umd.edu>
2019-09-17 23:32:20 -0400

    s;lfdkj

--- interp.rkt
+++ interp.rkt
@@ -14,15 +14,15 @@
          (interp e1)
          (interp e2))]
     [`(abs ,e0)
-     (abs e0)]
+     (abs (interp e0))]
     [`(- ,e0)
-     (- e0)]
+     (- (interp e0))]
     [(cons 'cond t) (interp-cond t)]
     ))
 
 (define (interp-cond e)
   (match (first e)
-    [`((zero? ,x) ,y) (if (zero? x) y (interp-cond (cdr e)))]
-    [`(else ,x) x]))
+    [`((zero? ,x) ,y) (if (zero? (interp x)) (interp y) (interp-cond (cdr e)))]
+    [`(else ,x) (interp x)]))
     
     
